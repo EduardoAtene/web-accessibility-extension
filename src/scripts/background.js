4d534deaac13489db7d8041631718cf3ff2d3f75
EduardoAtene <eduardoatenesilvae@gmail.com>
2024-10-18 22:25:19 -0300

    🧪 Test: Testando

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -182,6 +182,7 @@ function runAxeAnalysis() {
 
   function createButton(text, onClick) {
     const button = document.createElement('button');
+    
     button.textContent = text;
     button.addEventListener('click', onClick);
     button.style.cssText = `
